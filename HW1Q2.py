98c6ac71a228efed5ea2c23ac4927028385a740c
joannazhang88 <56102342+joannazhang88@users.noreply.github.com>
2020-04-25 11:00:44 -0500

    Create HW1Q2.py

---
+++ HW1Q2.py
@@ -0,0 +1,8 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.array as cl_array
+import pyopencl.clrandom as clrand
+import pyopencl.tools as cltools
+from pyopencl.scan import GenericScanKernel
+import matplotlib.pyplot as plt
+import time
