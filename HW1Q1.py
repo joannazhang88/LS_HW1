1f82389673ed886e33dde7a5ff205d4ae1fed946
joannazhang88 <56102342+joannazhang88@users.noreply.github.com>
2020-04-27 11:40:54 -0500

    Update HW1Q1.py

--- HW1Q1.py
+++ HW1Q1.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
-
 
 from mpi4py import MPI
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import numpy as np
 import time
 import scipy.stats as sts
-
-
-# In[ ]:
 
 
 comm = MPI.COMM_WORLD
@@ -19,47 +14,57 @@ rank = comm.Get_rank()
 size = comm.Get_size()
 
 
-# In[ ]:
+def sim_lifetime():
+    
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    t0 = time.time()
+
+    rho = 0.5
+    mu = 3.0
+    sigma = 1.0
+    z_0 = mu
 
 
-N = int(1000/size)
-t0 = time.time()
+    S = 1000 
+    T = int(4160)
+    np.random.seed(25)
 
-rho = 0.5 
-mu = 3.0 
-sigma = 1.0 
-z_0 = mu
-S = 1000
-T = int(4160)
-np.random.seed(25)
+    # scatter to processors
+    if rank == 0:
+        eps_mat = sts.norm.rvs(loc=0, scale=sigma, size=(S, T))
+    else:
+        eps_mat = None
+    eps_mat = comm.scatter(eps_mat, root=0)
 
-eps_mat = sts.norm.rvs(loc=0, scale=sigma, size=(T, N)) 
-z_mat = np.zeros((T, N))
-z_mat[0, :] = z_0
+    N = eps_mat.shape[0]
+    z_mat = np.zeros((N, T))
+    z_mat[0, :] = z_0
 
+    for s_ind in range(N):
+        z_tm1 = z_0
+        for t_ind in range(T):
+            e_t = eps_mat[s_ind, t_ind]
+            z_t = rho * z_tm1 + (1 - rho) * mu + e_t
+            z_mat[s_ind, t_ind] = z_t
+            z_tm1 = z_t
 
-for s_ind in range(N): 
-    z_tm1 = z_0
-    for t_ind in range(T):
-        e_t = eps_mat[t_ind, s_ind]
-        z_t = rho * z_tm1 + (1 - rho) * mu + e_t 
-        z_mat[t_ind, s_ind] = z_t
-        z_tm1 = z_t
+    # gather all simulations
+    z_all = None
+    if rank == 0:
+        z_all = np.empty([S, T], dtype='float')
+    comm.Gather(sendbuf = z_mat, recvbuf = z_all, root=0)
 
+    if rank == 0:
+        time_elapsed = time.time() - t0
+        print(time_elapsed)
+        #return time_elapsed
 
-# In[6]:
+    return
 
+def main():
+    sim_lifetime()
 
-#simulations = None
-if rank == 0:
-    comm.Gather(z_mat, root=0)
-
-
-# In[ ]:
-
-
-time_elapsed = time.time() - t0
-#print(time_elapsed)
-print("Simulated lifetimem in: %f seconds on %d MPI processes"
-                % (time_elapsed, size))   
-
+if __name__ == '__main__':
+    main()
